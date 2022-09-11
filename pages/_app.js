0943d14d615628e45c8b85ad0b1af6c3a316dd48
kenneduu <101065166+kenneduu@users.noreply.github.com>
2022-09-11 16:18:17 +0100

    Update 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Navbar />
+  <Component {...pageProps} /></>
 }
 
 export default MyApp
+
+
+/* https://www.youtube.com/watch?v=zokKTsRx7QA 49:28 11/9/22 */
